7e8fc6023393e3aa958c77fb09408de726c983e2
Chris Williams <chris@bingosnet.co.uk>
2012-12-18 10:32:43 +0000

    Import of BINGOS/Acme-MetaSyntactic-legobatman-0.02 from CPAN.
    gitpan-cpan-distribution: Acme-MetaSyntactic-legobatman
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         BINGOS/Acme-MetaSyntactic-legobatman-0.02.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

---
+++ lib/Acme/MetaSyntactic/legobatman.pm
@@ -0,0 +1,120 @@
+package Acme::MetaSyntactic::legobatman;
+{
+  $Acme::MetaSyntactic::legobatman::VERSION = '0.02';
+}
+
+#ABSTRACT: LEGO Batman theme
+
+use strict;
+use warnings;
+use Acme::MetaSyntactic::List;
+our @ISA = qw( Acme::MetaSyntactic::List );
+__PACKAGE__->init();
+
+q[You-Know-Who];
+
+=pod
+
+=head1 NAME
+
+Acme::MetaSyntactic::legobatman - LEGO Batman theme
+
+=head1 VERSION
+
+version 0.02
+
+=head1 DESCRIPTION
+
+Characters from the LEGO Batman and LEGO Batman2 games as gleaned from
+the L<http://www.gamefaqs.com/> walkthroughs.
+
+I<Kerpow!!>
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
+
+=head1 AUTHOR
+
+Chris Williams
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Chris Williams.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
+__DATA__
+# names
+Alfred
+Aquaman
+Bane
+Batgirl
+Batman
+Black_Canary
+Brainiac
+Bruce_Wayne
+Captain_Boomerang
+Catwoman
+Clark_Kent
+Clayface
+Clown_Goon
+Commissioner_Gordon
+Cyborg
+Dick_Grayson
+Fishmonger
+Freeze_Girl
+General_Zod
+Green_Lantern
+Harley_Quinn
+Hawkgirl
+Hawkman
+Huntress
+Hush
+Joker_Goon
+Joker_Henchman
+Killer_Croc
+Killer_Moth
+LexBot
+Lex_Luthor
+Lois_Lane
+Man_Bat
+Martian_Manhunter
+Military_Policeman
+Mime_Goon
+Mr_Freeze
+Nightwing
+Penguin_Goon
+Penguin_Henchman
+Penguin_Minion
+Poison_Ivy
+Poison_Ivy_Goon
+Police_Marksman
+Police_Officer
+Policeman
+Ras_al_Ghul
+Riddler_Goon
+Riddler_Henchman
+Robin
+SWAT
+Sailor
+Scientist
+Security_Guard
+Sinestro
+Supergirl
+Superman
+The_Flash
+The_Joker
+The_Mad_Hatter
+The_Penguin
+The_Riddler
+The_Scarecrow
+Two_Face
+Two_Face_Goon
+Vicki_Vale
+Wonder_Woman
+Yeti
+Zoo_Sweeper
